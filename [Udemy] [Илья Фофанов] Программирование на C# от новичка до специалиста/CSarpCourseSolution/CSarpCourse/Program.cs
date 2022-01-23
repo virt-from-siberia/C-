7898d5086a5f87eda171c3a3ecd82a16ee0e5205
Aleksey <Elchin>
2022-01-24 04:17:20 +0900

    C# notebook

--- [Udemy] [Илья Фофанов] Программирование на C# от новичка до специалиста/CSarpCourseSolution/CSarpCourse/Program.cs
+++ [Udemy] [Илья Фофанов] Программирование на C# от новичка до специалиста/CSarpCourseSolution/CSarpCourse/Program.cs
@@ -6,6 +6,134 @@ namespace CSarpCourse
     {
         static void Main(string[] args)
         {
+            string nameConcat = string.Concat("My ","name ", "is ", "Aleksey ");
+            Console.WriteLine(nameConcat);
+
+            nameConcat = string.Join(" ", "My", "name", "is", "Alex");
+            Console.WriteLine(nameConcat);
+
+            string newName = nameConcat.Insert(0, "by the way, ");
+            Console.WriteLine(newName);
+
+            nameConcat = nameConcat.Remove(5, 1);
+            Console.WriteLine(nameConcat);
+
+            string replcaed = nameConcat.Replace("n", "z");
+            Console.WriteLine(replcaed);
+
+            string data = "12;28:56:78:44:77";
+            string[] splitData = data.Split(";");
+            string onevalue = splitData[0];
+            Console.WriteLine(onevalue);
+
+            char[] chars = nameConcat.ToCharArray();
+            Console.WriteLine(chars[0]);
+
+        }
+        static void StringEmpinees()
+        {
+            string strEmpty = string.Empty;
+            string empty = "";
+            string whiteSpaced = " ";
+            string notEmpty = " x";
+            string nullString = null;
+
+            bool isNullOrEmpty = string.IsNullOrEmpty(nullString);
+            Console.WriteLine(isNullOrEmpty);
+
+            Console.WriteLine("==========");
+            bool isNullOrWhiteSpace = string.IsNullOrWhiteSpace(nullString);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(empty);
+            Console.WriteLine(isNullOrWhiteSpace);
+
+            isNullOrWhiteSpace = string.IsNullOrWhiteSpace(whiteSpaced);
+            Console.WriteLine(isNullOrWhiteSpace);
+        }
+
+        static void QueryingString()
+        {
+            string name = "lololoytgg";
+            bool containsA = name.Contains("a");
+            bool containsL = name.Contains("l");
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsL);
+
+            bool endsWithGG = name.EndsWith("gg");
+            Console.WriteLine(endsWithGG);
+
+            bool startsWith = name.StartsWith("lol");
+            Console.WriteLine(startsWith);
+
+            int indexOfA = name.IndexOf("t", 1);
+            Console.WriteLine(indexOfA);
+
+            int lastIndexOfA = name.LastIndexOf('o');
+            Console.WriteLine(lastIndexOfA);
+
+            Console.WriteLine(name.Length);
+
+            string subStr = name.Substring(5);
+            string subStr2 = name.Substring(0, 3);
+
+            Console.WriteLine(subStr);
+            Console.WriteLine(subStr2);
+        }
+
+        static void StaticAndInstanceMembers()
+        {
+            string name = new string("abra codabra");
+            bool containsA = name.Contains("a");
+            bool containsI = name.Contains("i");
+
+            Console.WriteLine(containsA);
+            Console.WriteLine(containsI);
+
+            string abc = string.Concat("a", "b", "c");
+            Console.WriteLine(abc);
+
+            Console.WriteLine(int.MinValue);
+
+            int x = 1;
+
+            string xStr = x.ToString();
+
+            Console.WriteLine(x);
+            Console.WriteLine(xStr);
+        }
+
+        static void Operators()
+        {
+            int x = 1;
+            x = x + 1;
+            x++;
+
+            Console.WriteLine(x);
+
+            x += 2;
+
+            Console.WriteLine(x);
+        }
+
+        static void Overflow()
+        {
+            checked
+            {
+                uint x = uint.MaxValue;
+                Console.WriteLine(x);
+
+                x = x + 1;
+                Console.WriteLine(x);
+
+                x = x - 1;
+                Console.WriteLine(x);
+            }
+        }
+
+        static void Literals()
+        {
             int x = 0b11;
             int y = 0b01001;
             int k = 0b0001001;
@@ -33,7 +161,33 @@ namespace CSarpCourse
 
             Console.WriteLine('\u0420');
             Console.WriteLine('\u0421');
-        } 
+        }
+
+        static void VariablesScopes()
+        {
+            var a = 1;
+
+            {
+                var b = 2;
+
+                {
+                    var c = 3;
+
+                    Console.WriteLine(a);
+                    Console.WriteLine(b);
+                    Console.WriteLine(c);
+                }
+
+                Console.WriteLine(a);
+                Console.WriteLine(b);
+                //Console.WriteLine(c);
+
+            }
+
+            Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+        }
 
         static void Variables()
         {
