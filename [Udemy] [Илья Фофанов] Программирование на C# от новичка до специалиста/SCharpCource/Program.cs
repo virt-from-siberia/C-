996210829becb9ad4940934b623ff3ada664082e
Aleksey <Elchin>
2022-05-13 16:31:13 +0900

    C# udemy fofanov notebook

---
+++ [Udemy] [Илья Фофанов] Программирование на C# от новичка до специалиста/SCharpCource/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SCharpCource
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
