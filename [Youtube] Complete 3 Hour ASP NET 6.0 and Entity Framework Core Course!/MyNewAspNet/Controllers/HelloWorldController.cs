dff45da29359ce98f96aa5084e0a7783000ac534
Aleksey <Elchin>
2022-07-17 01:03:47 +0900

    [Youtube] Complete 3 Hour ASP NET 6.0 and Entity Framework Core Course! notebook

---
+++ [Youtube] Complete 3 Hour ASP NET 6.0 and Entity Framework Core Course!/MyNewAspNet/Controllers/HelloWorldController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyNewAspNet.Models;
+
+namespace MyNewAspNet.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET
+        // public IActionResult Index()
+
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+
+        public IActionResult Index()
+        {
+            // DogViewModel doggo = new DogViewModel() {Name = "Sif", Age = 2};
+            return View(dogs);
+        }
+
+        public string Hello() 
+        {
+            return "Who is there ?";
+        }
+
+        public IActionResult createDog(DogViewModel dogViewModel)
+        {
+            Console.WriteLine($"dogViewModel, {dogViewModel}");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+    }
+}
